920597b3c93349c2a70e00aba8a0b3fd0d65cf1b
GArifullinII <ildaridrisovich@gmail.com>
2022-07-14 19:01:55 +0400

    Initial Commit

---
+++ people_quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  people_quiz
+//
+//  Created by Ildar Garifullin on 14/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
